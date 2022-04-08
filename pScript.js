649e1b48c4c443b8d85611d17f814c6e6ca92b55
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-04-08 10:45:45 -0500

    added basic background color

--- pScript.js
+++ pScript.js
@@ -1,7 +1,26 @@
 var canvas = document.getElementById("myCanvas");
 
+const resize = () => {
+    // make canvas size in full use
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resize();
+
+window.addEventListener('resize', resize);
 var canvasCtx = canvas.getContext("2d");
 
-canvasCtx.moveTo(0,0);
-canvasCtx.lineTo(100,100);
-canvasCtx.stroke();
+function fullScreen() {
+    var elmt = document.getElementById('myCanvas');
+
+    if(elmt.webkitRequestFullScreen) {
+        elmt.webkitRequestFullScreen();
+    } else {
+        elmt.mozRequestFullScreen();
+    }
+}
+
+canvas.addEventListener("click", fullScreen);
+
+
