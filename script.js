c6d12f67971adbcc8ff22926c049c398f9c0932b
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-04-25 14:01:49 -0500

    Added start screen

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-var canvas = document.getElementById("myCanvas");
+let gameDiv = document.querySelector("#startGame");
+const alphaBet = new Map();
 
-const resize = () => {
-    // make canvas size in full use
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+
+document.addEventListener("keypress", (e)=> {
+    populateAlphaBet();
+    if(intToChar(e.which) == 's') {
+        console.log("Game is going to start, pls wait!")
+        draw();
+    }
+});
+
+function populateAlphaBet(){
+    for (let i = 65; i < 123; i++) {
+        if(i < 91) {
+            let total = Math.abs(65-i);
+            alphaBet.set(i, String.fromCharCode('A'.charCodeAt(0) + total));
+        } else if (i > 96){
+            let total = Math.abs(97-i);
+            alphaBet.set(i, String.fromCharCode('a'.charCodeAt(0)+total));
+        }
+    }
 }
-
-resize();
-
-window.addEventListener('resize', resize);
-var canvasCtx = canvas.getContext("2d");
-
+// Converting Int to Char
+function intToChar(x) {
+    return alphaBet.get(x);
+}
 function draw() {
-    canvasCtx.beginPath();
-    canvasCtx.arc(100, 50, 10, 0, 2* Math.PI);
-    canvasCtx.closePath();
-    canvasCtx.fill();
 
-    window.requestAnimationFrame(draw);
 }
-// draw();
-canvasCtx.font = "30px Arial";
-canvasCtx.fillStyle = "red";
-canvasCtx.textAlign = "center";
-canvasCtx.fillText("Welcome to Self-Training Ping Pong", canvas.width/2, canvas.height / 2);
-
