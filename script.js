b625748713ab80646319b619f00819262960e72b
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-04-25 17:10:04 -0500

    added gameCanvas

--- script.js
+++ script.js
@@ -2,22 +2,47 @@ let gameDiv = document.querySelector("#startGame");
 let c = document.querySelector("#gameCanvas");
 let ctx = c.getContext("2d");
 
+
 const alphaBet = new Map();
 
 // Service worker to make the game installable
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function() {
-        console.log("Service worker is registered.")
+        console.log("Service worker is registered.");
     });
+}
+window.onload = function() {
+    console.log('loading...');
+    prepareDocument();
+    resizeCanvas();
+}
+
+window.onresize = function() {
+    console.log('resizing...');
+    resizeCanvas();
 }
 
 document.addEventListener("keypress", (e)=> {
     populateAlphaBet();
     if(intToChar(e.which) == 's') {
         console.log("Game is going to start, pls wait!")
-        draw();
+        const wlcTxt = document.querySelector("#startGame");
+        wlcTxt.remove();
+        drawOb();
     }
 });
+
+// 
+function prepareDocument(){
+    document.body.style.padding = "0px";
+    document.body.style.margin = "0px";
+}
+
+// Resizing the Canvas whenever we load it
+function resizeCanvas () {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+}
 
 function populateAlphaBet(){
     for (let i = 65; i < 123; i++) {
@@ -34,14 +59,21 @@ function populateAlphaBet(){
 function intToChar(x) {
     return alphaBet.get(x);
 }
-function draw() {
-    ctx.clearRect(0,0, c.clientWidth, c.height);
+
+function drawOb() {
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    let width = c.width * 0.05;
+    let height = c.height * 0.2;
+
+    let xpos = c.width * 0.1 - width/2;
+    let ypos = c.height/2 - height/2;
 
     ctx.beginPath();
-    ctx.arc(100,z,10,0,2*Math.PI);
+    ctx.rect(xpos, ypos, width, height);
     ctx.closePath();
-    ctx.fillStyle = "black";
-    ctx.fill();
 
-    window.requestAnimationFrame(draw);
+    ctx.fillStyle = "green"
+    ctx.fill();
+    window.requestAnimationFrame(drawOb);
 }
