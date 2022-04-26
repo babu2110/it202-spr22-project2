42b4f4818bd4906408bf6ef2d7a1d5fa363826d9
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-04-26 17:04:11 -0500

    save

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 let gameDiv = document.querySelector("#startGame");
 let c = document.querySelector("#gameCanvas");
 let ctx = c.getContext("2d");
+let countS = 0;
+let width = window.innerWidth * 0.05;
+let height = window.innerHeight * 0.2;
+console.log("This is width " + width);
+console.log("This is height " + height);
 
+let ball = {
+
+}
+let paddle = {
+    x: window.innerWidth * 0.1 - width/2,
+    y: window.innerHeight/2 - height/2,
+    w: window.innerWidth * 0.025,
+    h: window.innerHeight * 0.2,
+    color: "BLACK",
+    score: 0
+}
 
 const alphaBet = new Map();
 
@@ -25,13 +41,43 @@ window.onresize = function() {
 document.addEventListener("keypress", (e)=> {
     populateAlphaBet();
     if(intToChar(e.which) == 's') {
-        console.log("Game is going to start, pls wait!")
-        const wlcTxt = document.querySelector("#startGame");
-        wlcTxt.remove();
-        drawOb();
+        // remove welcome text from the start menu
+        if(countS < 1) {
+            console.log("Game is going to start, pls wait!")
+            const wlcTxt = document.querySelector("#startGame");
+            wlcTxt.remove();
+            countS++;
+        }
+        let color = "black";
+        let deltaY = 3;
+        console.log(paddle.x)
+        console.log(paddle.y)
+        console.log(paddle.w)
+        console.log(paddle.h)
+        console.log(paddle.color)
+        drawPaddle(paddle.x, paddle.y, paddle.w, paddle.h, paddle.color);
     }
 });
 
+//
+function drawText(x,y, text, color){
+    ctx.fillStyle = color;
+    ctx.font = "55px fantasy";
+    ctx.fillText(text, x,y);
+}
+
+// Drawing hitting pad
+function drawPaddle(x,y, w,h, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,w,h);
+}
+// Ping pong ball
+function drawCircle(x, y, r, color){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0, Math.PI*2, false);
+    ctx.fill();
+}
 // 
 function prepareDocument(){
     document.body.style.padding = "0px";
@@ -42,6 +88,14 @@ function prepareDocument(){
 function resizeCanvas () {
     c.width = window.innerWidth;
     c.height = window.innerHeight;
+    paddle = {
+        x: window.innerWidth * 0.1 - width/2,
+        y: window.innerHeight/2 - height/2,
+        w: window.innerWidth * 0.025,
+        h: window.innerHeight * 0.2,
+        color: "BLACK",
+        score: 0
+    }
 }
 
 function populateAlphaBet(){
@@ -60,20 +114,3 @@ function intToChar(x) {
     return alphaBet.get(x);
 }
 
-function drawOb() {
-    ctx.clearRect(0, 0, c.width, c.height);
-
-    let width = c.width * 0.05;
-    let height = c.height * 0.2;
-
-    let xpos = c.width * 0.1 - width/2;
-    let ypos = c.height/2 - height/2;
-
-    ctx.beginPath();
-    ctx.rect(xpos, ypos, width, height);
-    ctx.closePath();
-
-    ctx.fillStyle = "green"
-    ctx.fill();
-    window.requestAnimationFrame(drawOb);
-}
